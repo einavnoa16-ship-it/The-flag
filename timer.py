2aa4109c93de1a03f747cf8a29433bcf4f2b6a11
ohav <ohavbenaroya@gmail.com>
2026-09-09 11:54:29 +0300

    timer

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+start = time.time()
+count_down=60
+
+def count_down_func(t):
+    while count_down>0:
+
+
+end=time.time()
+
+length =end-start
+
+for i in range(length):
+    print()
