b1585f97f0a7f507f0584063475d69e4b1eced5f
agam2468el10-netizen <agam2468el10@gmail.com>
2026-09-09 09:33:41 +0300

    grass flag

--- grass.py
+++ grass.py
@@ -11,12 +11,14 @@ image = Image.open('pictures/grass.png')
 #image.show()
 
 def random_bosh(field,rows,cols):
+    list_p=[]
     mone=0
     while mone<20:
         r=random.randint(0,rows-1)
         c=random.randint(0,cols-1)
         if field[r][c]!="m" or field[r][c]!="f":
           field[r][c]="i"
+          list_p.append(list([r,c]))
           mone+=1
         else:
             continue
@@ -24,4 +26,4 @@ def random_bosh(field,rows,cols):
     for i in range(consts.BOARD_ROWS):
         print(field[i],end="\n")
 
-    return field
+    return list_p
